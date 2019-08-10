2d195654d58f91596154937eb502d0c6b37b0faa
Gabriel Martinez <reitaka@gmail.com>
2019-08-09 18:07:24 -0700

    Add a first sketch from a Processing tutorial

---
+++ src/bin/001_mouse_ellipse.rs
@@ -0,0 +1,30 @@
+// Based on the Getting Started Processing tutorial:
+// https://py.processing.org/tutorials/gettingstarted/
+
+use nannou::prelude::*;
+
+fn view(app: &App, frame: &Frame) {
+    // Begin drawing
+    let draw = app.draw();
+
+    // Draw an ellipse to follow the mouse.
+    let color = if app.mouse.buttons.left().is_down() {
+        BLACK
+    } else {
+        WHITE
+    };
+    // TODO: Is there any way to draw an outline on the ellipse? Like the default ellipse call in
+    // Processing does.
+    draw.ellipse()
+        .x_y(app.mouse.x, app.mouse.y)
+        .radius(80.0)
+        .color(color);
+
+    // Write the result of our drawing to the window's frame.
+    draw.to_frame(app, &frame)
+        .unwrap();
+}
+
+fn main() {
+    nannou::sketch(view);
+}
